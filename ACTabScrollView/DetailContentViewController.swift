b6c536667655c12492aa7139a2a6979c9f1629b2
ginppian <ginppian@gmail.com>
2016-11-10 23:28:17 -0600

    Vista Detallada

--- ACTabScrollView/DetailContentViewController.swift
+++ ACTabScrollView/DetailContentViewController.swift
@@ -8,30 +8,25 @@
 
 import UIKit
 
-class DetailContentViewController: UIViewController {
+class DetailContentViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
     var algo = String()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return 1
+    }
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellIdentifier", for: indexPath) as! DetailTableViewCell
+        cell.labelEvento.text = "hola mundo"
+        
+        return cell
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
---
+++ DetailTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  DetailTableViewCell.swift
+//  FIF 16
+//
+//  Created by ginppian on 10/11/16.
+//  Copyright © 2016 AzureChen. All rights reserved.
+//
+
+import UIKit
+
+class DetailTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var labelEvento: UILabel!
+    @IBOutlet weak var labelHora: UILabel!
+    @IBOutlet weak var labelPonente: UILabel!
+    @IBOutlet weak var labelPais: UILabel!
+    @IBOutlet weak var labelSede: UILabel!
+    @IBOutlet weak var labelDescripcion: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
